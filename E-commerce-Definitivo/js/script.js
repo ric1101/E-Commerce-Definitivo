c1d86c13db8bb51710face13c5e25415521f4a3e
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-04-30 14:30:59 +0200

    Inizio

---
+++ E-commerce-Definitivo/js/script.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
