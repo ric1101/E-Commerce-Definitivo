54e4731465a8deea0b7b36c109757d881ee5e253
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-05-10 12:18:33 +0200

    12.18

--- E-commerce-Definitivo/js/pagamento.js
+++ E-commerce-Definitivo/js/pagamento.js
@@ -1,29 +1,50 @@
 
-// let chiudi = document.querySelector('.close');
-// let paga = document.querySelector('.paga');
-// let modal = document.querySelector('.modal');
-// let richiesti = document.querySelectorAll('.richiesti');
+let chiudi = document.querySelector('.close');
+let paga = document.querySelector('.paga');
+let modal = document.querySelector('.modal');
+let richiesti = document.querySelectorAll('.richiesti');
 
-// function pagato() {
+console.log(richiesti);
+function pagato() {
     
-//     richiesti.forEach(element => {
-        
+    // richiesti.forEach(element => {
+    //     if (element.value != '') {
+    //         console.log(element.value);
+    //         modal.classList.add('d-block');
+    //     }
+    // });
 
 
-//     });
+}
 
-//     modal.classList.add('d-block');
-
-
-
-// }
-
-// function chiusura() {
+function chiusura() {
     
-//     modal.classList.remove('d-block');
-//     window.location.href = 'http://127.0.0.1:5500/index.html';
-// }
+    modal.classList.remove('d-block');
+    window.location.href = 'http://127.0.0.1:5500/index.html';
+}
 
-// paga.addEventListener('click', pagato);
+paga.addEventListener('click', pagato);
 
-// chiudi.addEventListener('click', chiusura);
+chiudi.addEventListener('click', chiusura);
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
