d5dfa6b6cfc52fd5f579af9d4f0c3fa629ed3898
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-05-12 19:27:10 +0200

    sistemato carrello

--- E-commerce-Definitivo/js/carrello.js
+++ E-commerce-Definitivo/js/carrello.js
@@ -1,7 +1,6 @@
 
 let divProdotti = document.querySelector('#div-prodotti');
 
-
 let nessunProdotto = `<div class="card rounded-3 mb-4" style="background-color: rgb(194, 193, 193);">
     <div class="card-body p-4">
         <div class="row d-flex justify-content-between align-items-center">
@@ -12,60 +11,61 @@ let nessunProdotto = `<div class="card rounded-3 mb-4" style="background-color: 
 
 let divTotaleOrdine = document.querySelector('#totale-ordini');
 
-async function popolaCarrello() {
+function popolaCarrello() {
     let price = 0;
     let arrayCarrello = JSON.parse(localStorage.getItem('arrayId'));
 
-
     if (arrayCarrello != null) {
+
         if (arrayCarrello.length > 0) {
             let carrelloHtml = '';
 
             console.log(arrayCarrello);
-            for (let i = 0; i < arrayCarrello.length; i++) {
+            arrayCarrello.forEach(element => {
+                const URLProdotto = `https://dummyjson.com/products/${element}`;
 
-                const URLProdotto = `https://dummyjson.com/products/${arrayCarrello[i]}`;
+                fetch(URLProdotto)
 
-                let data = await fetch(URLProdotto)
                     .then(res => res.json())
-                    
-                    carrelloHtml += `<div class="card rounded-3 mb-4">
-                    <div class="card-body p-4">
-                    <div class="row d-flex justify-content-between align-items-center">
-                    <div class="col-md-2 col-lg-2 col-xl-2">
-                    <img src="${data.images[0]}" class="img-fluid rounded-3" alt="Image">
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-3">
-                    <p class="lead fw-normal mb-2">${data.title}</p>
-                    </div>
-                    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                    <h5 class="mb-0">${data.price} €</h5>
-                    </div>
-                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                    <a href="#!" class="text-danger"><i class="cestino fas fa-trash fa-lg" data-id="${data.id}"></i></a>
-                    </div>
-                    </div>
-                    </div>
-                    </div>`;
+                    .then((data) => {
+                        let prodotto = `<div class="card rounded-3 mb-4">
+                <div class="card-body p-4">
+                <div class="row d-flex justify-content-between align-items-center">
+                <div class="col-md-2 col-lg-2 col-xl-2">
+                <img src="${data.images[0]}" class="img-fluid rounded-3" alt="Image">
+                </div>
+                <div class="col-md-3 col-lg-3 col-xl-3">
+                <p class="lead fw-normal mb-2">${data.title}</p>
+                </div>
+                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                <h5 class="mb-0">${data.price} €</h5>
+                </div>
+                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                <a href="#!" class="text-danger"><i class="cestino fas fa-trash fa-lg" data-id="${data.id}"></i></a>
+                </div>
+                </div>
+                </div>
+                </div>`;
+
+                        carrelloHtml += prodotto;
                         price += data.price;
-                        
-                    }
-                    divTotaleOrdine.innerHTML = `<h4> Totale ordine: ${price} € </h4>`;
-                    divProdotti.innerHTML = carrelloHtml;
-                    cancellaProdotto();
-                    
-                } else {
-                    divProdotti.innerHTML = nessunProdotto;
-                    divTotaleOrdine.innerHTML = `<h4> Totale ordine: ${price} € </h4>`;
-                }
-            } else {
-                divProdotti.innerHTML = nessunProdotto;
-                divTotaleOrdine.innerHTML = `<h4> Totale ordine: ${price} € </h4>`;
-            }
-            
+                        divTotaleOrdine.innerHTML = `<h4> Totale ordine: ${price} € </h4>`;
+                        divProdotti.innerHTML = carrelloHtml;
+                        cancellaProdotto();
+                    })
+
+            });
+        } else {
+            divProdotti.innerHTML = nessunProdotto;
+            divTotaleOrdine.innerHTML = `<h4> Totale ordine: ${price} € </h4>`;
         }
-        
-        
+    } else {
+        divProdotti.innerHTML = nessunProdotto;
+        divTotaleOrdine.innerHTML = `<h4> Totale ordine: ${price} € </h4>`;
+
+    }
+
+}
 popolaCarrello();
 
 function cancellaProdotto() {
@@ -93,16 +93,16 @@ let numeroArticoli = document.querySelector('#numeroArticoli');
 let numProdotti = 0;
 
 function contoCarrello() {
-  console.log(numProdotti);
+    console.log(numProdotti);
 
-  arrayCarrello = JSON.parse(localStorage.getItem('arrayId'));
+    arrayCarrello = JSON.parse(localStorage.getItem('arrayId'));
 
-  numProdotti = arrayCarrello.length;
-  numeroArticoli.innerHTML = numProdotti;
+    numProdotti = arrayCarrello.length;
+    numeroArticoli.innerHTML = numProdotti;
 
-  if (numProdotti == 0) {
-    numeroArticoli.innerHTML = null;
-  }
+    if (numProdotti == 0) {
+        numeroArticoli.innerHTML = null;
+    }
 
 }
 contoCarrello();
