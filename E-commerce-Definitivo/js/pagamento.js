58ffd713ff97280504d234eb7aefc8b7cfbb3005
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-05-12 17:48:58 +0200

    risolto form definitivo

--- E-commerce-Definitivo/js/pagamento.js
+++ E-commerce-Definitivo/js/pagamento.js
@@ -1,46 +1,47 @@
 
+let richiesti = document.querySelectorAll('.richiesti');
+let modal = document.querySelector('.modal');
 let chiudi = document.querySelector('.close');
 let paga = document.querySelector('.paga');
-let modal = document.querySelector('.modal');
-let richiesti = document.querySelectorAll('.richiesti');
+const form = document.querySelector('.needs-validation');
+const forms = document.querySelectorAll('.needs-validation');
+
 
 console.log(richiesti);
 function pagato() {
-    
-    richiesti.forEach(element => {
-        if (element.value == '') {
-            console.log(element.value);
-            modal.classList.add('d-none');
-          } else {
-            
-            modal.classList.add('d-block');
-        }
-    });
+  console.log('Funzione pagato chiamata');
+  richiesti.forEach(element => {
+    if (element.value == '') {
+      console.log(element.value);
 
+      modal.classList.add('d-none');
+    } else {
+
+      modal.classList.add('d-block');
+    }
+  });
 
 }
 
 function chiusura() {
-    
-    modal.classList.remove('d-block');
-    window.location.href = 'index.html';
+
+  modal.classList.remove('d-block');
+  window.location.href = 'index.html';
 }
-
-
 
 chiudi.addEventListener('click', chiusura);
 
-const forms = document.querySelectorAll('.needs-validation')
+// Aggiungi gestore di eventi click al pulsante "Paga"
+paga.addEventListener('click', function (event) {
+  event.preventDefault(); // Previeni l'invio predefinito del form
 
-Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          } else {
-            paga.addEventListener('click', pagato);
-          }
-          form.classList.add('was-validated')
-      }, false)
-  })    
-
+  // Esegui la validazione del form
+  if (form.checkValidity()) {
+    // Se il form è valido, mostra il modal
+    modal.classList.add('d-block');
+    pagato();
+  } else {
+    // Se il form non è valido, mostra gli errori di validazione
+    form.classList.add('was-validated');
+  }
+});
